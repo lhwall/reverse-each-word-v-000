aa29c43dac64b4a49bd2cde3d9649a46d3bc2505
Lucy Wall <lhwall@gmail.com>
2018-03-20 15:16:52 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   new_array = sentence.split(" ")
-  new_array.collect do |word|
-    word.reverse
+  (new_array.collect do |word|
+    word.reverse).join(" ")
 end
 end
 
