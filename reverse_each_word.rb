e0401482f5e4df9cad5ce464b066b12a7018d9a8
Lucy Wall <lhwall@gmail.com>
2018-03-20 15:24:02 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word(sentence)
+  new_array = sentence.split(" ")
+  (new_array.collect do |word|
+    word.reverse
+end).join(" ")
+end
+
+def reverse_each_word_each(sentence)
+  new_array = sentence.split(" ")
+  new_array_2 = []
+  new_array.each do |word|
+    new_array_2 << word.reverse
+end
+return new_array_2.join(" ")
+end
